daaf284c8d1686fba13a78e8932547ce7a9b9929
rohit raj <rohitrajchy03@gmail.com>
2026-08-27 19:00:56 +0530

    day06 strat

--- Day06-notes-app/server.js
+++ Day06-notes-app/server.js
@@ -1,6 +1,8 @@
-const app = require("./src/app")
+const app = require("./src/app");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 const port = 3000;
 
-app.listen(port,()=>{
-    console.log(`this server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`this server is running on port ${port}`);
+});
