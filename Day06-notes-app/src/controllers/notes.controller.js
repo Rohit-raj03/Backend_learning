0f84140df2b525c1496f6465f7ed45a50a7df53f
rohit raj <rohitrajchy03@gmail.com>
2026-09-03 07:09:42 +0530

    update and get sinlge note

--- Day06-notes-app/src/controllers/notes.controller.js
+++ Day06-notes-app/src/controllers/notes.controller.js
@@ -23,4 +23,48 @@ const getAllNotesController = async (req, res) => {
     console.log("this error form get all notes", error);
   }
 };
-module.exports = { notesCreaateController, getAllNotesController };
+//single note
+const getSingleNote = async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  try {
+    const singleNote = await notesModel.findById(id);
+    res.status(200).json({
+      message: "this is single user",
+      note: singleNote,
+    });
+  } catch (error) {
+    console.log("this is error form find by ID ", error);
+    res.status(500).json({
+      message: "Failed to get note",
+      error: error.message,
+    });
+  }
+};
+
+//updata data by id
+const updateNoteController = async (req, res) => {
+  const id = req.params.id;
+  const updateData = req.body;
+
+  try {
+    const updateNote = await notesModel.findByIdAndUpdate(id, updateData);
+    res.status(200).json({
+      message: "note update successfully",
+      note: updateData,
+    });
+  } catch (error) {
+    console.log("this is update error", error);
+    res.status(500).json({
+      message: "Faild to update note",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = {
+  notesCreaateController,
+  getAllNotesController,
+  getSingleNote,
+  updateNoteController,
+};
--- Day06-notes-app/src/routes/notes.routes.js
+++ Day06-notes-app/src/routes/notes.routes.js
@@ -2,11 +2,14 @@ const express = require("express");
 const {
   notesCreaateController,
   getAllNotesController,
+  getSingleNote,
+  updateNoteController,
 } = require("../controllers/notes.controller");
 
 const router = express.Router();
 console.log("this is router");
 router.post("/create", notesCreaateController);
 router.get("/allnotes", getAllNotesController);
-
+router.get("/onenote/:id", getSingleNote)
+router.put("/update/:id", updateNoteController)
 module.exports = router;
